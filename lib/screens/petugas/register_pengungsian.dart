7cdfa3f5567837f96f46f74402736acf8e93dc0e
Padliwinata <rpadliwinata@gmail.com>
2023-04-11 21:15:49 +0700

    feat: tambah halaman register pengungsian

---
+++ lib/screens/petugas/register_pengungsian.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class RegisterPengungsian extends StatelessWidget {
+  const RegisterPengungsian({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
