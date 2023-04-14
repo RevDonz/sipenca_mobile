59eeb5d61c45a110161c23834525041c830b8270
RevDonz <aprilio842@gmail.com>
2023-04-14 10:32:38 +0700

    Add routing by Named Routing in main.dart

---
+++ lib/screens/warga/warga.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:sipenca_mobile/screens/warga/home.dart';
+import 'package:sipenca_mobile/screens/warga/keluarga.dart';
+import 'package:sipenca_mobile/screens/warga/profile.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomePage(),
+    KeluargaPage(),
+    ProfilePage(),
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_rounded),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.groups_outlined),
+              label: 'Keluarga',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline),
+              label: 'Profile',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.indigoAccent,
+          onTap: _onItemTapped,
+        ));
+  }
+}
