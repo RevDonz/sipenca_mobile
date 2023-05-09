da89f644dbb2b0fb330979d77951a8416d6d9add
amaliayunaa <42332019+amaliayunaa@users.noreply.github.com>
2023-05-09 22:15:00 +0700

    menambah file auth untuk menyambungkan login ke firebase dan perubahan2 lainnya

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../../firebase/auth.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -10,8 +12,127 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   bool _rememberMe = false;
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _isObscured = true;
 
-  @override
+  void _toggleObscure() {
+    setState(() {
+      _isObscured = !_isObscured;
+    });
+  }
+
+  void _signIn() async {
+    User? user = await AuthService.signIn(
+        _emailController.text, _passwordController.text);
+    if (user != null) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Center(
+              child: Column(
+                children: [
+                  Icon(
+                    Icons.check_circle_rounded,
+                    color: Colors.green,
+                    size: 50,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Berhasil !",
+                    style: TextStyle(color: Colors.green, fontSize: 20),
+                  ),
+                  Text(
+                    "Anda berhasil masuk",
+                    style: TextStyle(color: Color(0xFF5C5C5C), fontSize: 18),
+                  )
+                ],
+              ),
+            ),
+            actionsPadding: EdgeInsets.only(bottom: 20),
+            actions: [
+              Center(
+                child: TextButton(
+                  child: Text(
+                    "Ok",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.normal),
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, "/");
+                  },
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    backgroundColor: Colors.indigoAccent,
+                  ),
+                ),
+              )
+            ],
+          );
+        },
+      );
+    } else {
+      // User gagal login
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Center(
+              child: Column(
+                children: [
+                  Icon(
+                    Icons.check_circle_rounded,
+                    color: Colors.red,
+                    size: 50,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Gagal !",
+                    style: TextStyle(color: Colors.red, fontSize: 20),
+                  ),
+                  Text(
+                    "Anda gagal masuk",
+                    style: TextStyle(color: Color(0xFF5C5C5C), fontSize: 18),
+                  )
+                ],
+              ),
+            ),
+            actionsPadding: EdgeInsets.only(bottom: 20),
+            actions: [
+              Center(
+                child: TextButton(
+                  child: Text(
+                    "Ok",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.normal),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context, "");
+                    _emailController.clear();
+                    _passwordController.clear();
+                  },
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    backgroundColor: Colors.indigoAccent,
+                  ),
+                ),
+              )
+            ],
+          );
+        },
+      );
+    }
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
@@ -21,9 +142,8 @@ class _LoginPageState extends State<LoginPage> {
                 child: Center(
           child: SingleChildScrollView(
               child: Container(
-            padding:
-                const EdgeInsets.only(top: 10, right: 20, left: 20, bottom: 10),
-            margin: const EdgeInsets.all(20),
+            padding: EdgeInsets.only(top: 10, right: 20, left: 20, bottom: 10),
+            margin: EdgeInsets.all(20),
             child:
                 Column(mainAxisAlignment: MainAxisAlignment.center, children: [
               Row(
@@ -42,7 +162,7 @@ class _LoginPageState extends State<LoginPage> {
                   )
                 ],
               ),
-              const Padding(padding: EdgeInsets.only(top: 30)),
+              Padding(padding: EdgeInsets.only(top: 30)),
               Column(children: [
                 SvgPicture.asset(
                   'assets/registerlogo.svg',
@@ -53,24 +173,31 @@ class _LoginPageState extends State<LoginPage> {
               Column(mainAxisAlignment: MainAxisAlignment.center, children: [
                 const Padding(padding: EdgeInsets.only(top: 50)),
                 TextFormField(
-                  //tambahkan icon email disini
+                  controller: _emailController,
                   decoration: InputDecoration(
                       prefixIcon: const Icon(Icons.email),
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10)),
                       hintText: 'Email'),
-
                   keyboardType: TextInputType.emailAddress,
                 ),
                 const Padding(padding: EdgeInsets.only(top: 20)),
                 TextFormField(
-                  //tambahkan icon kunci disini
+                  controller: _passwordController,
                   decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.lock),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                      hintText: 'Kata Sandi'),
-                  obscureText: true,
+                    prefixIcon: const Icon(Icons.lock),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _isObscured ? Icons.visibility : Icons.visibility_off,
+                        color: Colors.grey,
+                      ),
+                      onPressed: _toggleObscure,
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    hintText: 'Kata Sandi',
+                  ),
+                  obscureText: _isObscured,
                 ),
               ]),
               const Padding(padding: EdgeInsets.only(top: 30)),
@@ -87,18 +214,72 @@ class _LoginPageState extends State<LoginPage> {
                           });
                         },
                       ),
-                      const Text('Ingat Saya'),
+                      Text('Ingat Saya'),
                     ],
                   ),
-                  const Padding(padding: EdgeInsets.only(top: 10)),
-                  SizedBox(
-                    width: 700, // ukuran lebar button
-                    height: 50, // ukuran tinggi button
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, "/");
-                      },
-                      style: ElevatedButton.styleFrom(
+                  Container(
+                      width: 700, // ukuran lebar button
+                      height: 50,
+                      // ukuran tinggi button
+                      child: ElevatedButton(
+                        onPressed: () {
+                          _signIn();
+                          // showDialog(
+                          //   context: context,
+                          //   builder: (BuildContext context) {
+                          //     return AlertDialog(
+                          //       title: Center(
+                          //         child: Column(
+                          //           children: [
+                          //             Icon(
+                          //               Icons.check_circle_rounded,
+                          //               color: Colors.green,
+                          //               size: 50,
+                          //             ),
+                          //             SizedBox(height: 10),
+                          //             Text(
+                          //               "Berhasil !",
+                          //               style: TextStyle(
+                          //                   color: Colors.green, fontSize: 20),
+                          //             ),
+                          //             Text(
+                          //               "Anda berhasil masuk",
+                          //               style: TextStyle(
+                          //                   color: Color(0xFF5C5C5C),
+                          //                   fontSize: 18),
+                          //             )
+                          //           ],
+                          //         ),
+                          //       ),
+                          //       actionsPadding: EdgeInsets.only(bottom: 20),
+                          //       actions: [
+                          //         Center(
+                          //           child: TextButton(
+                          //             child: Text(
+                          //               "Ok",
+                          //               style: TextStyle(
+                          //                   color: Colors.white,
+                          //                   fontSize: 18,
+                          //                   fontWeight: FontWeight.normal),
+                          //             ),
+                          //             onPressed: () {
+                          //               Navigator.pushNamed(context, "/");
+                          //             },
+                          //             style: TextButton.styleFrom(
+                          //               shape: RoundedRectangleBorder(
+                          //                 borderRadius:
+                          //                     BorderRadius.circular(10),
+                          //               ),
+                          //               backgroundColor: Colors.indigoAccent,
+                          //             ),
+                          //           ),
+                          //         )
+                          //       ],
+                          //     );
+                          //   },
+                          // );
+                        },
+                        style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.indigoAccent,
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(10),
