25e289e3c174f792414953b2daa7392d471d32a5
Zahrandi Rusrizal <Zahrandi@student.telkomuniversity.ac.id>
2023-05-16 23:14:27 +0700

    Memisahkan fungsi penerimaan warga kedalam file accWarga.dart serta menghubungkan petugas dengan profile.dart

--- lib/screens/petugas/petugas.dart
+++ lib/screens/petugas/petugas.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:sipenca_mobile/components/appBar.dart';
 import 'package:sipenca_mobile/firebase/pengungsian.dart';
+import 'package:sipenca_mobile/firebase/auth.dart';
+import 'package:sipenca_mobile/screens/warga/profile.dart';
+import 'package:sipenca_mobile/screens/petugas/accWarga.dart';
 
 class ListPengungsi extends StatefulWidget {
   const ListPengungsi({super.key});
@@ -11,6 +14,8 @@ class ListPengungsi extends StatefulWidget {
 
 class _ListPengungsiState extends State<ListPengungsi> {
   List<Map<String, dynamic>> dataPengungsi = [];
+  Map<String, dynamic>? profilePetugas;
+  int _selectedIndex = 0;
 
   void getListPengungsi() async {
     List<Map<String, dynamic>> list =
@@ -20,23 +25,40 @@ class _ListPengungsiState extends State<ListPengungsi> {
       dataPengungsi = list;
     });
   }
+
+  void getProfile() async {
+    Map<String, dynamic>? userData =
+        await DatabaseService.getDetailUsers(AuthService.getCurrentUserID());
+
+    setState(() {
+      profilePetugas = userData;
+    });
+  }
+
+  void checkProfile() async {
+    Map<String, dynamic>? userData =
+        await DatabaseService.getDetailUsers(AuthService.getCurrentUserID());
+
+    if (userData!['full_name'] == "" ||
+        userData['nik'] == "" ||
+        userData['no_hp'] == "" ||
+        userData['jenis_kelamin'] == "" ||
+        userData['tgl_lahir'] == "" ||
+        userData['alamat'] == "") {
+      setState(() {
+        _selectedIndex = 2;
+      });
+    }
+  }
   // List<Map<String, dynamic>> dataPengungsi = [
   //   {"nama": "Rama", "member": 4, "alamat": "Kampung A", "jarak": 500},
   //   {"nama": "Doni", "member": 2, "alamat": "Kampung B", "jarak": 1000},
   //   {"nama": "Nopal", "member": 3, "alamat": "Kampung C", "jarak": 1500},
   // ];
 
-  int _selectedIndex = 0;
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Scaffold(),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-    Scaffold(),
-  ];
+
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -47,11 +69,22 @@ class _ListPengungsiState extends State<ListPengungsi> {
   void initState() {
     super.initState();
     getListPengungsi();
+    getProfile();
   }
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> widgetOptions = <Widget>[
+      accWarga(listWarga: dataPengungsi),
+      const Text(
+        'Index 2: School',
+        style: optionStyle,
+      ),
+      ProfilePage(profileWarga: profilePetugas),
+    ];
+
     return Scaffold(
+      body: Center(child: widgetOptions.elementAt(_selectedIndex)),    
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.white,
         elevation: 0,
@@ -64,79 +97,15 @@ class _ListPengungsiState extends State<ListPengungsi> {
             icon: Icon(Icons.business),
             label: 'Pengungsian',
           ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: 'Profile',
+          ),
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.indigoAccent,
         onTap: _onItemTapped,
       ),
-      body: SafeArea(
-          child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(children: [
-            const AppBarSipenca(role: "Petugas"),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [Text('Pengungsi yang datang')],
-            ),
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: dataPengungsi.length,
-              itemBuilder: (context, index) {
-                // String jarak;
-                // if (dataPengungsi[index]["jarak"] >= 1000) {
-                //   jarak = "${dataPengungsi[index]["jarak"] / 1000} KM";
-                // } else {
-                //   jarak = "${dataPengungsi[index]["jarak"]} M";
-                // }
-                return GestureDetector(
-                  onTap: () {},
-                  child: Card(
-                    elevation: 0,
-                    shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(16))),
-                    child: InkWell(
-                      hoverColor: Colors.transparent,
-                      borderRadius: const BorderRadius.all(Radius.circular(16)),
-                      onTap: () {},
-                      child: Container(
-                        padding: const EdgeInsets.all(20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(dataPengungsi[index]['full_name']),
-                                // Text(jarak),
-                                // Text("${dataPengungsi[index]['member']}orang"),
-                              ],
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 16),
-                              child: Text(dataPengungsi[index]['alamat']),
-                            ),
-                            FloatingActionButton(
-                              heroTag: "btnPengungsi$index",
-                              onPressed: () {},
-                              backgroundColor: Colors.indigoAccent,
-                              elevation: 5,
-                              shape: const RoundedRectangleBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(12))),
-                              child: const Icon(Icons.input),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                );
-              },
-            )
-          ]),
-        ),
-      )),
     );
   }
 }
