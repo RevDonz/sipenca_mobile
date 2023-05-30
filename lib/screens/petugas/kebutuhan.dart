51add727a17f197d2c208b807683afeed7aec8d0
RevDonz <aprilio842@gmail.com>
2023-05-30 19:35:51 +0700

    create read kebutuhan

--- lib/screens/petugas/kebutuhan.dart
+++ lib/screens/petugas/kebutuhan.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:sipenca_mobile/firebase/kebutuhan.dart';
 
 import '../../components/appBar.dart';
 import '../../firebase/pengungsian.dart';
@@ -18,12 +19,17 @@ class _DetailPengungsianState extends State<DetailPengungsian> {
   TextEditingController namaController = TextEditingController();
   TextEditingController kapasitasController = TextEditingController();
   TextEditingController descController = TextEditingController();
+  TextEditingController kebutuhanController = TextEditingController();
+  TextEditingController kategoriController = TextEditingController();
 
   Map<String, dynamic>? dataPengungsi = {};
   Map<String, dynamic>? dataPengungsiBaru = {};
-  bool isLoading = true;
 
-  void getDataPengungsian() async {
+  List<Map<String, dynamic>> listKebutuhan = [];
+  bool isLoading = true;
+  String kategori = 'primer';
+
+  Future<void> getDataPengungsian() async {
     Map<String, dynamic>? data = await DatabaseService.getPengungsianById(
         widget.profileData!['pengungsian']);
 
@@ -34,10 +40,22 @@ class _DetailPengungsianState extends State<DetailPengungsian> {
     });
   }
 
+  void getDataKebutuhan() async {
+    List<Map<String, dynamic>> list = [];
+
+    list = await NeedsService.getAllKebutuhan();
+
+    setState(() {
+      listKebutuhan = list;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
-    getDataPengungsian();
+    getDataPengungsian().then((value) {
+      getDataKebutuhan();
+    });
   }
 
   @override
@@ -298,8 +316,99 @@ class _DetailPengungsianState extends State<DetailPengungsian> {
                               ),
                             ),
                           ),
-                    Center(
-                      child: Text("Kedua"),
+                    Scaffold(
+                      body: Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: ListView.builder(
+                          itemCount: listKebutuhan.length,
+                          itemBuilder: (context, index) {
+                            return Card(
+                              child: Text(listKebutuhan[index]['kebutuhan']),
+                            );
+                          },
+                        ),
+                      ),
+                      floatingActionButton: FloatingActionButton(
+                          onPressed: () {
+                            // NeedsService.addKebutuhan(
+                            //     "kebutuhan", "kategori", "doc");
+                            showDialog<String>(
+                              context: context,
+                              builder: (BuildContext context) => AlertDialog(
+                                title: const Text('Update Data'),
+                                content: Builder(builder: (context) {
+                                  return Container(
+                                    // height: height - 300,
+                                    // width: width - 200,
+                                    child: SingleChildScrollView(
+                                      child: ListBody(
+                                        children: [
+                                          Container(
+                                            margin: const EdgeInsets.symmetric(
+                                                vertical: 10),
+                                            child: TextField(
+                                              controller: kebutuhanController,
+                                              decoration: InputDecoration(
+                                                border: OutlineInputBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            10)),
+                                                labelText: 'Nama Kebutuhan',
+                                              ),
+                                            ),
+                                          ),
+                                          Container(
+                                            margin: const EdgeInsets.symmetric(
+                                                vertical: 10),
+                                            child:
+                                                DropdownButtonFormField<String>(
+                                              decoration: InputDecoration(
+                                                border: OutlineInputBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            10)),
+                                                labelText: 'Kategori',
+                                              ),
+                                              items: const [
+                                                DropdownMenuItem<String>(
+                                                    value: "primer",
+                                                    child: Text("Primer")),
+                                                DropdownMenuItem<String>(
+                                                    value: "sekunder",
+                                                    child: Text("Sekunder")),
+                                              ],
+                                              value: kategori,
+                                              onChanged: (value) {
+                                                kategori = value!;
+                                              },
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                }),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () =>
+                                        Navigator.pop(context, 'Cancel'),
+                                    child: const Text('Tutup'),
+                                  ),
+                                  TextButton(
+                                    onPressed: () async {
+                                      NeedsService.addKebutuhan(
+                                          kebutuhanController.text, kategori);
+                                      getDataKebutuhan();
+                                      Navigator.pop(context, 'OK');
+                                    },
+                                    child: const Text('Update'),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                          backgroundColor: Colors.indigoAccent,
+                          child: const Icon(Icons.add)),
                     )
                   ],
                 ),
