78cadf4a8e7dd1415ef8482232a4c9ef359e03fc
RevDonz <aprilio842@gmail.com>
2023-06-05 22:17:49 +0700

    fix beberapa bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sipenca_mobile/firebase_options.dart';
 import 'package:sipenca_mobile/screens/admin/admin.dart';
 import 'package:sipenca_mobile/screens/auth/login.dart';
@@ -11,12 +10,11 @@ import 'package:sipenca_mobile/screens/warga/warga.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
   const MyApp({super.key});
 
   // This widget is the root of your application.
